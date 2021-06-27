3729c1c6bf777c2b3abc685bc53ed03421eec6ac
BradwellC <59516796+BradwellC@users.noreply.github.com>
2021-06-27 07:40:40 +0100

    main content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CampgroundComponent } from './campground/campground.component';
+import { CommentComponent } from './comment/comment.component';
+import { ReviewComponent } from './review/review.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CampgroundComponent,
+    CommentComponent,
+    ReviewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
